8eefbdf6a0eaeaea936cbb59140e5eee1e581b6f
กิตติ อำนาจสมบูรณ์ <105689839+kittiumnatsomboon@users.noreply.github.com>
2024-10-29 16:10:43 +0700

    datepicker

--- public/js/datepicker.js
+++ public/js/datepicker.js
@@ -1,3 +1,8 @@
 $( function() {
-    $( "#dateofbirth" ).datepicker();
+    $( "#dateofbirth" ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "-100:+0",
+        dateFormat: 'yy-mm-dd'
+    });
 });
